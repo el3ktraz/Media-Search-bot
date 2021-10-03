c976484b87bd85ba73414a9be9e33b1eff739c59
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-10-03 17:36:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -145,7 +145,7 @@ async def group(client, message):
                     [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}")]
                 )
         else:
-            await client.send_text(chat_id=message.from_user.id, text='Sorry, No Movie Related to the Given Word Was Found 🥺\n\nPlease Go to Google and Confirm the Correct Spelling 🙏')
+            await client.send_message(chat_id=message.from_user.id, text='Sorry, No Movie Related to the Given Word Was Found 🥺\n\nPlease Go to Google and Confirm the Correct Spelling 🙏')
             return
             
         if not btn:
