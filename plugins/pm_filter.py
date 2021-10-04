d333458f102fadd2475f299037436dac6fe212c7
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-10-04 22:16:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -147,11 +147,11 @@ async def group(client, message):
         else:
             await message.reply(quote=True,text="<b>Sorry, No Movie/Series Related to the Given Word Was Found 🥺</b>\n\n<b>Please Go to Google and Confirm the Correct Spelling 🙏</b>")
             reply_markup=InlineKeyboardMarkup(
-                 [
-                     [
-                         InlineKeyboardButton("🔍 Search 🔎", url='https://www.google.com')
-                     ]
-                 ]
+                [
+                    [
+                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                    ]
+                ]
             )
             return
             
