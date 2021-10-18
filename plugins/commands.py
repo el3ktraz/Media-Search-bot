27ec109739ad090af807a587c5fb801c44975676
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-10-18 08:12:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -202,7 +202,7 @@ async def bot_info(bot, message):
             InlineKeyboardButton("⭕️ 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ⭕️", url="https://t.me/tvseriezzz_update")
         ],
         [
-            InlineKeyboardButton("⚙️ 𝙷𝚎𝚕𝚙 ⚙️", callback_data="help")
+            InlineKeyboardButton("⚙️ 𝙷𝚎𝚕𝚙 ⚙️", callback_data='help')
         ]
         ]
     await message.reply(text="<b>Developer : <a href='https://t.me/MrC_VENOM'>MrC《》VENOM</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSupport Group : <a href='https://t.me/tvseriezzz'>Click here</a>\nUpdate Channel : <a href='https://t.me/tvseriezzz_update'>♠️ 𝑨𝒍𝒍 𝑰𝒏 𝑶𝒏𝒆 𝑮𝒓𝒐𝒖𝒑 {Update}</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
@@ -215,7 +215,7 @@ async def bot_info(bot, message):
             InlineKeyboardButton("⭕️ 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ⭕️", url="https://t.me/tvseriezzz_update")
         ],
         [
-            InlineKeyboardButton("𝙰𝚋𝚘𝚞𝚝 🚩", callback_data="about")
+            InlineKeyboardButton("𝙰𝚋𝚘𝚞𝚝 🚩", callback_data='about')
         ]
         ]
     await message.reply(text="<b>If You Have Any Doubts And If Any Errors or Bugs Inform Us On Our Support Group ❗️\n Use Below Buttons To Get Support Group / Update channel Links </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
