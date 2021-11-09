256fb36d0bc6ae3ccc257acf16ac97ca1c0ba506
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-09 17:58:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -348,7 +348,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_text(
-                text=script.START_TXT,
+                text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
                 reply_markup=reply_markup,
                 parse_mode='html'
             )
@@ -364,7 +364,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_text(
-                text=script.HELP_TXT,
+                text=script.HELP_TXT.format(query.from_user.mention),
                 reply_markup=reply_markup,
                 parse_mode='html'
             )
