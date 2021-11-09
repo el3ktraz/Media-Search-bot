5f16379b6979e355f020db1bf684a8a800fc0872
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-09 13:59:16 +0530

    Create p_ttishow.py

---
+++ plugins/p_ttishow.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from info import ADMINS
+from utils import get_size
+from Script import script
+
+
+@Client.on_message(filters.command('stats') & filters.incoming)
+async def get_ststs(bot, message):
+    rju = await message.reply('Fetching stats..')
+    total_users = await db.total_users_count()
+    totl_chats = await db.total_chat_count()
+    files = await Media.count_documents()
+    size = await db.get_db_size()
+    free = 536870912 - size
+    size = get_size(size)
+    free = get_size(free)
+    await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
