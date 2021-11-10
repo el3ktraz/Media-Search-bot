ad47eedf4fc9d0e51a249b15ddd4c14e2cd72e36
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-10 11:14:51 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,9 +1,9 @@
 import logging
 from pyrogram import Client, emoji, filters
-from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
-
-from utils import get_search_results, is_subscribed
+from database.ia_filterdb import get_search_results
+from utils import is_subscribed, get_size
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
@@ -32,20 +32,20 @@ async def answer(bot, query):
 
     offset = int(query.offset or 0)
     reply_markup = get_reply_markup(query=string)
-    files, next_offset = await get_search_results(string,
+    files, next_offset, total = await get_search_results(string,
                                                   file_type=file_type,
                                                   max_results=10,
                                                   offset=offset)
 
     for file in files:
         title=file.file_name
-        size=file.file_size
+        size=get_size(file.file_size)
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
                 f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
             except Exception as e:
-                print(e)
+                logger.exception(e)
                 f_caption=f_caption
         if f_caption is None:
             f_caption = f"{file.file_name}"
@@ -58,18 +58,25 @@ async def answer(bot, query):
                 reply_markup=reply_markup))
 
     if results:
-        switch_pm_text = f"{emoji.FILE_FOLDER} Results"
+        switch_pm_text = f"{emoji.FILE_FOLDER} Results - {total}"
         if string:
             switch_pm_text += f" for {string}"
-
-        await query.answer(results=results,
+        try:
+            await query.answer(results=results,
                            is_personal = True,
                            cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="start",
                            next_offset=str(next_offset))
+        except QueryIdInvalid:
+            pass
+        except Exception as e:
+            logging.exception(str(e))
+            await query.answer(results=[], is_personal=True,
+                           cache_time=cache_time,
+                           switch_pm_text=str(e)[:63],
+                           switch_pm_parameter="error")
     else:
-
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
         if string:
             switch_pm_text += f' for "{string}"'
@@ -86,18 +93,13 @@ def get_reply_markup(query):
         [
             InlineKeyboardButton('❣ Share ❣', url='http://t.me/share/url?url=https://t.me/tvseriezzz'),
             InlineKeyboardButton('🎬 Update 🎬', url='https://t.me/tvseriezzz_update')
+        ],
+        [
+            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
 
 
-def get_size(size):
-    """Get size in readable format"""
 
-    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
-    size = float(size)
-    i = 0
-    while size >= 1024.0 and i < len(units):
-        i += 1
-        size /= 1024.0
-    return "%.2f %s" % (size, units[i])
+
