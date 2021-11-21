b1f4a781a69e02eda0b5bf3a2258095fe3763af7
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-21 22:01:32 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -194,23 +194,6 @@ def extract_user(message: Message) -> Union[int, str]:
         user_first_name = message.from_user.first_name
     return (user_id, user_first_name)
 
-def last_online(from_user):
-    time = ""
-    if from_user.is_bot:
-        time += "🤖 Bot :("
-    elif from_user.status == 'recently':
-        time += "Recently"
-    elif from_user.status == 'within_week':
-        time += "Within the last week"
-    elif from_user.status == 'within_month':
-        time += "Within the last month"
-    elif from_user.status == 'long_time_ago':
-        time += "A long time ago :("
-    elif from_user.status == 'online':
-        time += "Currently Online"
-    elif from_user.status == 'offline':
-        time += datetime.fromtimestamp(from_user.last_online_date).strftime("%a, %d %b %Y, %H:%M:%S")
-    return time
 
 async def is_subscribed(bot, query):
     try:
