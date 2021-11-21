728f332eadedc536981d5e713036755184916a13
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-21 19:33:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,7 +9,7 @@ from pyrogram.errors import UserNotParticipant
 logger = logging.getLogger(__name__)
 
 @Client.on_message(filters.command("start"))
-async def start(bot, cmd):
+async def start(bot, cmd, message):
     usr_cmdall1 = cmd.text
     if usr_cmdall1.startswith("/start subinps"):
         if AUTH_CHANNEL:
